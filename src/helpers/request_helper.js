79380fef982a5a6bc364539ee669f456180b8ad0
Nyalls Hemingway <nyallshemingway@Nyallss-MacBook-Pro.local>
2018-10-26 10:51:31 +0100

    completed basic setup, including public and src directories - all empty

---
+++ src/helpers/request_helper.js
@@ -0,0 +1,26 @@
+const Request = function (url) {
+  this.url = url;
+};
+
+Request.prototype.get = function () {
+  return fetch(this.url)
+    .then( (response) => response.json() );
+};
+
+// Request.prototype.get = function (onComplete, onError) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', this.url);
+//   xhr.send();
+//
+//   xhr.addEventListener('load', () => {
+//     if (xhr.status !== 200) {
+//       onError(xhr.status);
+//       return;
+//     }
+//
+//     const jsonString = xhr.responseText;
+//     const data = JSON.parse(jsonString);
+//     onComplete(data);
+//   });
+// };
+module.exports = Request;
